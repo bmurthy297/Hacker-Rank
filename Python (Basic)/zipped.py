b17bcfb0c96f13f00baea6710e661dec10ed0479
Bhanu Murthy Mummidi <bmurthy.mummidi97@gmail.com>
2025-10-04 00:55:43 +0530

    added

--- Python (Basic)/zipped.py
+++ Python (Basic)/zipped.py
@@ -1,2 +1,12 @@
 # Zipped : https://www.hackerrank.com/challenges/zipped/problem
 
+n, x = map(int, input().split())  
+
+marks = []  
+for _ in range(x):
+    marks.append(map(float, input().split()))
+
+
+for student in zip(*marks):
+    print(sum(student)/x)
+
