35603076029fe7b24e7416c5838047d8231d614a
Bhanu Murthy Mummidi <bmurthy.mummidi97@gmail.com>
2025-10-19 15:18:48 +0530

    added

---
+++ Python (Intermediate)/decorators_2_name_directory.py
@@ -0,0 +1,20 @@
+# Decorators 2 - Name Directory : https://www.hackerrank.com/challenges/decorators-2-name-directory/problem
+
+import operator
+
+def person_lister(f):
+    def inner(people):
+        # complete the function
+        people.sort(key = lambda x : int(operator.itemgetter(2)(x)))
+        
+        return [f(person) for person in people]
+        
+    return inner
+
+@person_lister
+def name_format(person):
+    return ("Mr. " if person[3] == "M" else "Ms. ") + person[0] + " " + person[1]
+
+if __name__ == '__main__':
+    people = [input().split() for i in range(int(input()))]
+    print(*name_format(people), sep='\n')
